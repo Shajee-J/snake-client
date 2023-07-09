00b94d832a04351eb100da3804a4dc4c2e0cc3a4
Shajee-j <Shajeejm@gmail.com>
2023-07-08 22:35:23 -0400

    export setupInput function and input listener

--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 const connect = require("./client.js")
-
+const input = require("./input.js")
 
 
 console.log("Connecting ...");
 connect();
 
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput()
+input.setupInput()
